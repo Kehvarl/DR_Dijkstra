61a248485086a94f045adb776d70be52c40f8428
Kehvarl <Kehvarl@Kehvarl.com>
2022-11-26 18:53:25 -0700

    Create initial setup

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,10 @@
-def tick args
-  args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
-  args.outputs.labels  << [640, 420, 'Join the Discord! https://discord.dragonruby.org', 5, 1]
-  args.outputs.sprites << [576, 280, 128, 101, 'dragonruby.png']
-end
+# Main.RB
+# Primary file checked by Dragon Ruby
+# Load all class and setup files first, and `Tick.RB` last.
+
+# Load classes here
+require 'app/game.rb'
+
+
+# Do this last so you know all your classes are loaded before the game starts
+require 'app/tick.rb'
