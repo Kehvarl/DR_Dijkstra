31ecbe8052cb611021cdcd0309f107e7398a02d7
Kehvarl <Kehvarl@Kehvarl.com>
2022-11-26 19:11:23 -0700

    Tile Class for map

--- app/game.rb
+++ app/game.rb
@@ -1,3 +1,14 @@
+class Tile
+  attr_sprite
+  def initialize args
+    self.w = args.w || 32
+    self.h = args.h || 32
+    self.x = args.x || 0
+    self.y = args.y || 0
+    self.path = args.path || 'sprites/tile/wall-0000.png'
+  end
+end
+
 class GameMap
   attr_accessor :w, :h
 
@@ -9,7 +20,7 @@ class GameMap
     (0..h).each |y|  do
       self.tiles[y] = []
       (0..w).each |x| do
-        self.tiles[y][x] = 10
+        self.tiles[y][x] = Tile.new({x: x, y: x})
       end
     end
   end
