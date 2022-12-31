fce46251972d7d9bc4063e2bad20af23790f3c08
Kehvarl <Kehvarl@Kehvarl.com>
2022-12-30 21:00:50 -0700

    still not a working D Map

--- app/tick.rb
+++ app/tick.rb
@@ -1,10 +1,10 @@
-def tick args
-  args.state.gmap ||= GameMap.new({})
-  if args.tick_count <= 0
-    args.state.gmap.loadfile('app/map.dat')
-  end
-  args.outputs.primitives  << args.state.gmap.render()
-end
+#def tick args
+#  args.state.gmap ||= GameMap.new({})
+#  if args.tick_count <= 0
+#    args.state.gmap.loadfile('app/map.dat')
+#  end
+#  args.outputs.primitives  << args.state.gmap.render()
+#end
 
 # TODO: Create Game Map
 # TODO: Create Movable Entity
@@ -12,3 +12,11 @@ end
 # TODO: Create FoV
 # TODO: Dijkstra Map
 # TODO: Use DM to move E to each goal in view
+
+
+def tick args
+  args.state.d ||= Dijkstra.new({})
+  args.state.d.goals << [5,5]
+  args.state.d.calc_map()
+  args.outputs.solids << args.state.d.render()
+end
